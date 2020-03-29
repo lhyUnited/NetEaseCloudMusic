bd04533c8c1a364fbe9527a9ad90738985fff252
李帮耀 <lhy_1314@yeah.net>
2020-03-29 19:10:35 +0800

    Added 404 page

---
+++ src/components/error/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+    <el-container>
+      <el-main>
+        <h1>404 NOT FOUND</h1>
+        <el-link type="primary" href="/">回到首页</el-link>
+      </el-main>
+    </el-container>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import HomePage from '@/components/HomePage'
+import NotFound from '@/components/error/NotFound'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // }
+    {
+      path: '/',
+      name: 'HomePage',
+      component: HomePage
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
+    }
   ]
 })
