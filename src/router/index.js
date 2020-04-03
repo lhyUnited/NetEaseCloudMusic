2103bc26fae9daed0a88b0e19d98d3b5b614fb28
李帮耀 <lhy_1314@yeah.net>
2020-04-03 13:05:58 +0800

    actually 'history' mode is also supported

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import SearchResults from '@/components/SearchResults'
 Vue.use(Router)
 
 export default new Router({
-  // mode: 'history',
+  mode: 'history',
   routes: [
     {
       path: '/',
