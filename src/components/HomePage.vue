2178f639b3ed8afa8d469c1098b5044173d2b6b3
李帮耀 <lhy_1314@yeah.net>
2020-04-23 14:02:48 +0800

    download link changed

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -11,7 +11,8 @@
           <ol>
             <li>由于实际调用的是网易云官方api，所以搜素结果与官网保持一致</li>
             <li>本站提供普通用户在线听歌和歌曲下载的功能<em>(vip歌曲受限)</em>，请勿将下载文件用于商业用途</li>
-            <li>本站所有功能在<el-link type="primary" href="https://static.tomluvjerry.cn:3001/uploads/b847a7a57f8d99672e367de2d78dec17.exe">chrome</el-link>, <el-link type="primary" href="https://static.tomluvjerry.cn:3001/uploads/635a6b13d17d6aed0269f09faf92a254.exe">firefox</el-link>和<el-link type="primary" href="https://static.tomluvjerry.cn:3001/uploads/b847a7a57f8d99672e367de2d78dec17.exe">搜狗高速浏览器</el-link>上测试通过</li>
+            <li>本站所有功能在<el-link type="primary" href="
+https://static.tomluvjerry.cn/files/lee@tomluvjerry.cn/b47a7b33e42cf7c246dd8b770d890f11.exe">chrome</el-link>, <el-link type="primary" href="https://static.tomluvjerry.cn/files/lee@tomluvjerry.cn/c63c683b1f6003194e7eed7614051128.exe">firefox</el-link>和<el-link type="primary" href="https://static.tomluvjerry.cn/files/lee@tomluvjerry.cn/6e555ae83734d90654db3d91fce20246.exe">搜狗高速浏览器</el-link>上测试通过</li>
             <li>iOS设备存在无法调节音量问题</li>
             <li>请使用<em>大陆ip访问</em>, 部分歌曲受地区保护</li>
           </ol>
