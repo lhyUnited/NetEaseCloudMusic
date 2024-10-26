0c41f3d5e421d416e5cbcbbfef2ae0a42b2a9209
lhyUnited <lhy_1314@yeah.net>
2024-10-27 01:37:46 +0800

    feat: UX enhancement

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -4,12 +4,24 @@
     <el-main class="nec-search-result">
       <el-skeleton animated :loading="loading" variant="rect" :throttle="500">
         <template #template>
-          <el-skeleton-item style="width: 100%; height: 30px;"></el-skeleton-item>
-          <el-skeleton-item style="width: 100%; height: 30px;"></el-skeleton-item>
-          <el-skeleton-item style="width: 100%; height: 30px;"></el-skeleton-item>
-          <el-skeleton-item style="width: 100%; height: 30px;"></el-skeleton-item>
-          <el-skeleton-item style="width: 100%; height: 30px;"></el-skeleton-item>
-          <el-skeleton-item style="width: 100%; height: 30px;"></el-skeleton-item>
+          <el-skeleton-item
+            style="width: 100%; height: 30px;"
+          ></el-skeleton-item>
+          <el-skeleton-item
+            style="width: 100%; height: 30px;"
+          ></el-skeleton-item>
+          <el-skeleton-item
+            style="width: 100%; height: 30px;"
+          ></el-skeleton-item>
+          <el-skeleton-item
+            style="width: 100%; height: 30px;"
+          ></el-skeleton-item>
+          <el-skeleton-item
+            style="width: 100%; height: 30px;"
+          ></el-skeleton-item>
+          <el-skeleton-item
+            style="width: 100%; height: 30px;"
+          ></el-skeleton-item>
         </template>
         <div>
           <el-table
@@ -17,11 +29,14 @@
             :header-cell-style="{
               'text-align': 'center'
             }"
-            :cell-style="{'text-align': 'center'}"
+            :cell-style="{ 'text-align': 'center' }"
           >
             <el-table-column label="名称">
               <template slot-scope="props">
-                <el-link type="primary" @click="getUrl(props.row.id, props.row)">
+                <el-link
+                  type="primary"
+                  @click="getUrl(props.row.id, props.row)"
+                >
                   <span>{{ props.row.name }}</span>
                 </el-link>
               </template>
@@ -48,10 +63,10 @@
             </el-table-column>
             <el-table-column label="艺人">
               <template slot-scope="props">
-              <span v-for="(item, index) in props.row.artists" :key="item.id">
-                {{ item.name }}
-                <span v-if="index < props.row.artists.length - 1">/</span>
-              </span>
+                <span v-for="(item, index) in props.row.artists" :key="item.id">
+                  {{ item.name }}
+                  <span v-if="index < props.row.artists.length - 1">/</span>
+                </span>
               </template>
             </el-table-column>
           </el-table>
@@ -116,12 +131,12 @@ export default {
       this.axios
         .get(
           '/search?keywords=' +
-          this.keyWord +
-          '&limit=' +
-          this.limit +
-          '&offset=' +
-          (this.offset - 1) +
-          '&type=1'
+            this.keyWord +
+            '&limit=' +
+            this.limit +
+            '&offset=' +
+            (this.offset - 1) +
+            '&type=1'
         )
         .then(res => {
           if (res.data.code === 200) {
@@ -147,34 +162,37 @@ export default {
     search () {
       this.initialList()
     },
-    getUrl (id, row, orderChange = true) {
-      this.axios
-        .get('/check/music?id=' + id)
-        .then(() => {
-          this.axios
-            .get('/song/url?id=' + id)
-            .then(res => {
-              if (res.data.code === 200) {
-                if (res.data.data[0].url === null) {
-                  this.$message.error('出于vip限制，未获取到mp3地址')
-                } else {
-                  this.songInfo.id = id
-                  this.songInfo.url = res.data.data[0].url
-                  this.songInfo.name = row.name
-                  this.songInfo.artists = row.artists
-                  this.$emit('getSongInfo', this.songInfo, orderChange)
-                }
-              }
-            })
-            .catch(err => {
-              console.log(err)
-              this.$message.error('网络出错啦')
-            })
-        })
-        .catch(err => {
-          console.log(err)
-          this.$message.error('亲爱的,暂无版权')
-        })
+    async getUrl (id, row, orderChange = true) {
+      const valid = await this.checkMusic(id)
+      if (!valid) return
+      await this.getSongInfo(id, row, orderChange)
+    },
+    async checkMusic (id) {
+      try {
+        const { data } = await this.axios.get(`/check/music?id=${id}`)
+        if (!data.success) this.$message.error(data.message)
+        return data.success
+      } catch (error) {
+        this.$message.error(error)
+      }
+    },
+    async getSongInfo (id, row, orderChange) {
+      try {
+        const { data } = await this.axios.get(`/song/url?id=${id}`)
+        if (data.code === 200) {
+          if (data.data[0].url === null) {
+            this.$message.error('出于vip限制，未获取到mp3地址')
+          } else {
+            this.songInfo.id = id
+            this.songInfo.url = data.data[0].url
+            this.songInfo.name = row.name
+            this.songInfo.artists = row.artists
+            this.$emit('getSongInfo', this.songInfo, orderChange)
+          }
+        }
+      } catch (error) {
+        this.$message.error(error)
+      }
     }
   }
 }
